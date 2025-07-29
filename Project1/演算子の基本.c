db189f81b18fdb27bf8e9824470214f7262f0165
kayabuk1 <yokosuka0609@gmail.com>
2025-07-29 14:15:08 +0900

    演算子の基本とコメント練習

---
+++ Project1/演算子の基本.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ƒRƒƒ“ƒg—ûK
+*/
+
+int main(void) 
+{
+	printf("%d + %d = %d\n", 5, 2, 5 + 2);//‘«‚µZ
+	printf("%d-%d=%d\n", 5, 2, 5 - 2);//ˆø‚«Z
+	printf("%d*%d=%d\n", 5, 2, 5 * 2);//Š|‚¯Z
+	printf("%d/%d=%d —]‚è %d\n", 5, 2, 5 / 2, 5 % 2);//Š„‚èZ‚Æ—]‚è
+}
