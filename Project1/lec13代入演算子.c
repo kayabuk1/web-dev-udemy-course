829a625be92a0f34fcedbe87681cfb0e50b194dd
kayabuk1 <yokosuka0609@gmail.com>
2025-07-30 18:26:48 +0900

    20250730今日の学習の最初代入演算子

--- Project1/lec13代入演算子.c
+++ Project1/lec13代入演算子.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 
 int main(void) {
+	int a1 = 2, b1 = 2, c1 = 2, d1 = 2;
+	int a2 = 2, b2 = 2, c2 = 2, d2 = 2;
 
-}
+	a1 = a1 + 1;
+	b1 = b1 - 1;
+	c1 = c1 * 2;
+	d1 = d1 / 2;
+
+	a2 += 1;
+	b2 -= 1;
+	c2 *= 2;
+	d2 /= 2;
+	printf("a1=%d b1 = % d c1 = % d d1 = % d\n", a1, b1, c1, d1);
+	printf("a2=%d b2 = % d c2 = % d d2 = % d\n", a2, b2, c2, d2);
+}/*
+ long_and_complex_variable_name = long_and_complex_variable_name + another_long_variable_name / some_value;
+ long_and_complex_variable_name += another_long_variable_name / some_value;
+ ’·‚¢•Ï”–¼‚Ì‚ÉŒø‰Ê‚ğ”­Šö‚·‚é
+ */
