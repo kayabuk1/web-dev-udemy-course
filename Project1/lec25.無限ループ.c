2fa81c605c03a6c28ce0dc624955d68d761ff862
kayabuk1 <yokosuka0609@gmail.com>
2025-08-06 15:12:17 +0900

    20250806調子悪い時でも始めれば作業興奮で集中することが出来るからプログラミングは良いね。return 0;を書くかどうか

---
+++ Project1/lec25.無限ループ.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void) {
+	int num;
+	printf("負の値で、ループから抜けます。\n");
+	while (1) {
+		printf("数値を入力：");
+		if (scanf("%d", &num) == 1) {
+			if (num < 0) {
+				break;
+			}
+		}
+		else {
+			printf("無効な入力です。もう一度入力してください。\n");
+			while (getchar() != '\n');
+		}
+	}
+	printf("終了\n");
+	return 0;
+	//C99 以降 main 関数の最後に return 文がない場合、
+	//自動的に return 0; が補われることが標準で定められたので無くても良いが、
+	//明示的に return 0; と書くことが推奨されているとのこと。
+}
