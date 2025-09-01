ddf181c5c731c57adfca5dc13b3368d5a15882c5
kayabuk1 <yokosuka0609@gmail.com>
2025-09-01 16:46:52 +0900

    0901取り敢えず再開。自分のペースでやっていくしかない。for文の直後の一行だけが有効範囲。

--- Project1/lec28-5.配列の初期化復習.c
+++ Project1/lec28-5.配列の初期化復習.c
@@ -2,27 +2,28 @@
 #include<stddef.h>
 #define array_size 6
 
-int main(void){
+int main(void) {
 	int n[] = { 5,4,3,2,1,0 };
 	int i;
-	for (i = 0; i<7; i++) {
+	for (i = 0; i < 7; i++)
+		//c言語におけるfor文のルールとして、for文のスコープ、つまり有効範囲は、
+		//「直後の１行だけ」と言うルールがある。なので1行であれば｛｝は不要。
+		//逆に｛｝で囲んでいるのは、｛｝で一行とコンパイラに認識させるため。
+		//つまり、｛｝で囲む方がイレギュラーとも言える。
 		printf("n[%d]=%d,", i, n[i]);
-	}
 	printf("\n");
 
 	int m[array_size] = { 5,4,3,2,1,0 };
 	size_t j;
-	for (j = 0; j < array_size; j++) {
+	for (j = 0; j < array_size; j++)
 		printf("m[%zu]=%d,", j, m[j]);
-	}
 	printf("\n");
 
 	int o[] = { 5,4,3,2,1,0 };
 	int k;
 	size_t array_size2 = sizeof(o) / sizeof(o[0]);
-	for (k = 0; k < array_size2; k++) {
+	for (k = 0; k < array_size2; k++)
 		printf("o[%d]=%d,", k, o[k]);
-	}
 	printf("\n");
 	return 0;
 }
